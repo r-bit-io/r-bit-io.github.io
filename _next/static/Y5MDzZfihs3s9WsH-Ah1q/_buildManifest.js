4a8b33cd3d83ec1487615eedbed2da217578afc7
Abdelkrim Bournane <abdelkrim.bournane@gmail.com>
2022-07-06 10:00:05 +0200

    Published

---
+++ _next/static/Y5MDzZfihs3s9WsH-Ah1q/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/248-5fb934b00231db0a.js","static/css/604930c73154a4b3.css","static/chunks/pages/index-d7911d176f10e402.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
